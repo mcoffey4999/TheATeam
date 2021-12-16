3a750fc72edd1b3f8908e94461960a5ac731a89a
Michael Coffey <michaelcoffey1999@gmail.com>
2021-12-16 13:40:54 -0600

    Added a find item function

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+LinkedList<String> list = new LinkedList<String>();
+    bool exit = false;
+        //AddFirst
+        //AddLast
+        list.AddLast("Milk");
+        list.AddLast("Eggs");
+        list.AddLast("Cheese");
+        list.AddFirst("Meat");
+        list.AddLast("Veg");
+        list.AddLast("Fruit");
+  
+        Console.WriteLine("Shopping List: \n");
+  
+        
+        foreach(string item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        
+        
+        while(!exit) {
+        Console.WriteLine("What would you like to do today?");
+        Console.WriteLine("1. Add item");
+        Console.WriteLine("2. Find an item");
+        Console.WriteLine("x. Exit List");
+
+        string input = Console.ReadLine();
+            switch (input) {
+                case "1":
+                Console.WriteLine("Put what you'd like to add");
+                string item = Console.ReadLine();
+
+                list.AddLast(item);
+                break;
+                case "2":
+                Console.WriteLine("Type the specific item.");
+                string nitem = Console.ReadLine();
+                if(list.Contains(nitem) == true){
+                    Console.WriteLine(nitem + " was found in the list!");
+                }
+                else{
+                    Console.WriteLine("Can't find " + nitem);
+                }
+                break;
+                case "x":
+                exit = true;
+                break;
+            }
+        }
+
+        //AddAfter
+        //AddBefore
+        LinkedListNode<string> node = list.Find("Meat");
+        list.AddAfter(node, "a)lunch meats");
+         foreach(string item in list)
+        {
+            Console.WriteLine(item);
+        }
+        
