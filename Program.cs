a94eeb89e255673c1d136ac2b9609b464e0c6b63
Michael Coffey <michaelcoffey1999@gmail.com>
2021-12-17 09:19:37 -0600

    Expanded code

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ LinkedList<String> list = new LinkedList<String>();
         while(!exit) {
         Console.WriteLine("What would you like to do today?");
         Console.WriteLine("1. Add item");
-        Console.WriteLine("2. Find an item");
+        Console.WriteLine("2. Find item");
+        Console.WriteLine("3. Delete item");
         Console.WriteLine("x. Exit List");
 
         string input = Console.ReadLine();
@@ -42,6 +43,15 @@ LinkedList<String> list = new LinkedList<String>();
                 }
                 else{
                     Console.WriteLine("Can't find " + nitem);
+                }
+                break;
+                case "3":
+                Console.WriteLine("State the item that will be removed");
+                string foundItem = Console.ReadLine();
+                list.Remove(foundItem);
+                Console.WriteLine("The item has been deleted");
+                foreach(string food in list){
+                    Console.WriteLine(food);
                 }
                 break;
                 case "x":
